15519ff950810f06de856967f9f31ac7fbfa077d
Łukasz Kuk <lukasz010420@gmail.com>
2023-03-12 16:18:11 +0100

    email form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -4,6 +4,14 @@ import streamlit as st
 class ContactMe:
     def __init__(self):
         st.header("Contact Me")
+        self._email_form()
+
+    @staticmethod
+    def _email_form():
+        with st.form(key="email_form"):
+            user_email = st.text_input("Your emial address")
+            message = st.text_area("Your message")
+            button = st.form_submit_button()
 
 
 if __name__ == "__main__":
